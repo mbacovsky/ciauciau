333d54a70a761d39d4ced356ad831d0bf82c1648
Tomas Strachota <tstrachota@redhat.com>
2013-01-21 18:22:17 +0100

    fix in configuring test db (cherry picked from commit b0680a1e3991b1b0f2ab594ac7ec2ba247689e6a)

--- steps/600-setup-katello-devel.sh
+++ steps/600-setup-katello-devel.sh
@@ -5,5 +5,7 @@ sed \
 	-e 's/[# ]*\(BUNDLER_EXT_NOSTRICT\)=.*/\1=1/g' \
 	-i /etc/sysconfig/katello
 
-cp /etc/katello/katello.template.yml ~/katello/src/config/katello.yml
+cp /etc/katello/katello.yml ~/katello/src/config/katello.yml
 #sed -e 's/shhhh/katello/g' -i ~/katello/src/config/katello.yml
+chown root:root ~/katello/src/config/katello.yml
+chmod 644 ~/katello/src/config/katello.yml
--- steps/605-setup-test-db.sh
+++ steps/605-setup-test-db.sh
@@ -1,6 +1,6 @@
 vim +2 /etc/katello/katello.yml 2> /dev/null <<EOF
 /test:
-:r !grep '^  database:' -A 6 /etc/katello/katello.yml |sed -s 's/katelloschema/katellotest/'
+:r !cat /etc/katello/katello.yml | grep -e '^production:' -A 10 -m 1 | grep -e '^  database:' -A 3 | sed -s 's/katelloschema/katellotest/'
 :wq
 EOF
 
