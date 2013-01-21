b96f6d6c92b3ce16c16658697ad5084690196a72
Tomas Strachota <tstrachota@redhat.com>
2013-01-21 17:26:26 +0100

    katello devel setup fixes (cherry picked from commit e86ac95272c1f8abfd23ac0596e264d7e565bf09)

--- steps/600-setup-katello-devel.sh
+++ steps/600-setup-katello-devel.sh
@@ -4,3 +4,6 @@ sed \
 	-e 's/[# ]*\(KATELLO_LOGGING\)=.*/\1=debug/g' \
 	-e 's/[# ]*\(BUNDLER_EXT_NOSTRICT\)=.*/\1=1/g' \
 	-i /etc/sysconfig/katello
+
+cp /etc/katello/katello.template.yml ~/katello/src/config/katello.yml
+#sed -e 's/shhhh/katello/g' -i ~/katello/src/config/katello.yml
--- steps/602-setup-devel-config.sh
+++ steps/602-setup-devel-config.sh
@@ -3,7 +3,7 @@ SCRIPT_DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cp $SCRIPT_DIR/../scripts/katello-config.template.sh ~/katello-config.sh
 cat >> ~/.bashrc <<'EOF'
-KATELLO_CONFIG_FILE="~/katello-config.sh"
+KATELLO_CONFIG_FILE=~/katello-config.sh
 EOF
 
 source ~/.bashrc
