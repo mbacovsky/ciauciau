223c0d518e27a192e737a8e89e05e3541bdb91a9
Tomas Strachota <tstrachota@redhat.com>
2013-02-04 10:58:01 +0100

    plan to setup foreman for development - fixes (cherry picked from commit 50715ba0ed682b05c49c89758efa8d44bd3cd81c)

--- steps/622-setup-foreman-devel.sh
+++ steps/622-setup-foreman-devel.sh
@@ -1,9 +1,11 @@
 FOREMAN_DIR=~/foreman/
 
-cp $FOREMAN_FIR/config/settings.yaml.example $FOREMAN_DIR/config/settings.yaml
+cp $FOREMAN_DIR/config/settings.yaml.example $FOREMAN_DIR/config/settings.yaml
 
 cp /etc/foreman/database.yml $FOREMAN_DIR/config/database.yml
 cat /etc/foreman/database.yml | sed 's/production:/development:/' >> $FOREMAN_DIR/config/database.yml
+
+mkdir $FOREMAN_DIR/tmp/
 
 pushd $FOREMAN_DIR
 bundle install --without mysql mysql2 sqlite libvirt --path vendor
