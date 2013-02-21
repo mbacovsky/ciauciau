bcb19d742615ad94a1a8d9aff8f480ce9766d45e
Tomas Strachota <tstrachota@redhat.com>
2013-02-21 16:01:26 +0100

    updated install katello devel (cherry picked from commit 542b9c46b3d9926741618abfe9b162b8d9080b0b)
    Conflicts:
    	steps/401-install-katello-devel.sh

--- steps/401-install-katello-devel.sh
+++ steps/401-install-katello-devel.sh
@@ -1,3 +1,7 @@
-yum install -y katello-devel-all
-yum install -y gcc-c++ gcc make ruby-devel postgresql-devel sqlite-devel libxml2 libxml2-devel libxslt libxslt-devel spacewalk-pylint
+#katello devel dependencies
+yum install -y katello-devel-all spacewalk-pylint python-pep8 python-mock
+#foreman devel dependencies
+yum install -y gcc-c++ gcc make ruby-devel postgresql-devel sqlite-devel libxml2 libxml2-devel libxslt libxslt-devel 
+#katello build dependencies
+yum install -y rubygem-alchemy asciidoc
 
