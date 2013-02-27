b4bcc1dacc6c86c7446a45985ad6727ca06de81a
Tomas Strachota <tstrachota@redhat.com>
2013-02-27 14:00:51 +0100

    katello configure use explicit password (cherry picked from commit 6840c8b27834421986469ded3d92050407afd64c)

--- steps/500-configure-headpin.sh
+++ steps/500-configure-headpin.sh
@@ -1,2 +1,2 @@
 echo "katello" > /root/my-oauth-token-file
-katello-configure --deployment=headpin $KATELLO_CONFIGURE_OPTIONS --oauth-token-file=/root/my-oauth-token-file
+katello-configure --deployment=headpin $KATELLO_CONFIGURE_OPTIONS --oauth-token-file=/root/my-oauth-token-file --user-pass=admin
--- steps/500-configure-sam.sh
+++ steps/500-configure-sam.sh
@@ -1,2 +1,2 @@
 echo "katello" > /root/my-oauth-token-file
-katello-configure --deployment=sam $KATELLO_CONFIGURE_OPTIONS --oauth-token-file=/root/my-oauth-token-file
+katello-configure --deployment=sam $KATELLO_CONFIGURE_OPTIONS --oauth-token-file=/root/my-oauth-token-file --user-pass=admin
